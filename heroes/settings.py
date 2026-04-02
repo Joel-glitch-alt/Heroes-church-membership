1f2455d702b7d18a730e5416028a9d105e613420
Joel-glitch-alt <joeladdition@gmail.com>
2026-04-02 12:52:40 +0000

    updated

--- heroes/settings.py
+++ heroes/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-af)nx$k-sr6l5y91o!_wc8ke24fllbz#66*i871_0z4+eg0n%n'
+SECRET_KEY = config('SECRET_KEY')   #'django-insecure-af)nx$k-sr6l5y91o!_wc8ke24fllbz#66*i871_0z4+eg0n%n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)       #True
 
 ALLOWED_HOSTS =  ['127.0.0.1', 'localhost', '0.0.0.0']      #['heroessummit.onrender.com']
 
@@ -77,16 +79,27 @@ WSGI_APPLICATION = 'heroes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         #'ENGINE': 'django.db.backends.sqlite3',
+#         #'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'heroes_church',
+#         'USER': 'myuser',
+#         'PASSWORD': 'pass@123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'heroes_church',
-        'USER': 'myuser',
-        'PASSWORD': 'pass@123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 
